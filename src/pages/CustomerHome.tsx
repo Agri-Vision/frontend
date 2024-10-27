a26ee009b370f295555ef3fd1cb6f1ed1d2eebbf
prathila01 <prathila01@gmail.com>
2024-10-27 17:11:19 +0530

    fix:home page adjestment

--- src/pages/CustomerHome.tsx
+++ src/pages/CustomerHome.tsx
@@ -14,6 +14,9 @@ return (
       <CssBaseline />
       <div className="header"><Statistics /></div>
       <h2 style={{ textAlign: 'left', fontFamily: 'Nunito, Poppins, sans-serif', marginBottom: '20px' }}>Estates</h2>
+      <div className="header"><ProjectsCompleted /></div>
+
+{/*       
       <div className="header"><Projects /></div>
 
       <h2 style={{ textAlign: 'left', fontFamily: 'Nunito, Poppins, sans-serif', marginBottom: '10px' }}>New Estates</h2>
@@ -23,7 +26,7 @@ return (
       <div className="header"><ProjectsPending /></div>
 
       <h2 style={{ textAlign: 'left', fontFamily: 'Nunito, Poppins, sans-serif', marginBottom: '10px' }}>Completed Estates</h2>
-      <div className="header"><ProjectsCompleted /></div>
+      <div className="header"><ProjectsCompleted /></div> */}
     </div>
 );
 };
